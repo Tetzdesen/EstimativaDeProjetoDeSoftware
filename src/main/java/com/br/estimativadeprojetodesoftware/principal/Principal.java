3a53a6a5599e2a785ea58cc37d2ea7330c8d2c08
Tetzdesen <Tetzdesen@gmail.com>
2025-02-02 17:57:11 -0300

    Lançamento do projeto inicial

---
+++ src/main/java/com/br/estimativadeprojetodesoftware/principal/Principal.java
@@ -0,0 +1,12 @@
+package com.br.estimativadeprojetodesoftware.principal;
+
+/**
+ *
+ * @author tetzner
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
