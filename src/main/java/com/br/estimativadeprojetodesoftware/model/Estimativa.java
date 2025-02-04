aad6878b69762bd3dafaae89279bc78fe3568ab8
Tetzdesen <Tetzdesen@gmail.com>
2025-02-04 15:49:31 -0300

    Remodelando a model - parte 2 - alterações das classes

--- src/main/java/com/br/estimativadeprojetodesoftware/model/Estimativa.java
+++ src/main/java/com/br/estimativadeprojetodesoftware/model/Estimativa.java
@@ -3,30 +3,21 @@ package com.br.estimativadeprojetodesoftware.model;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 public class Estimativa {
-    private UUID idEstimativa;
+    private int id;
     private Map<String, Double> campos;
 
     public Estimativa() {
-        idEstimativa = UUID.randomUUID();
         campos = new HashMap<>();
     }
 
-    public UUID getIdEstimativa() {
-        return idEstimativa;
+    public int getId() {
+        return id;
     }
 
     public Map<String, Double> getCampos() {
         return Collections.unmodifiableMap(campos);
-    }
-
-    public void setIdEstimativa(UUID idEstimativa) {
-        if(idEstimativa == null){
-            throw new RuntimeException("Erro: id da estimativa nulo");
-        }
-        this.idEstimativa = idEstimativa;
     }
 
     public void adicionarCampo(String chave, double valor) {
