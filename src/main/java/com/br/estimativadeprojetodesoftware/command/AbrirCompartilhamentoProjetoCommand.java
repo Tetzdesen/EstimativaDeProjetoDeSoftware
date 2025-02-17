238326ea2d208620ebb1b1aa3464d4cfc31333da
Tetzdesen <Tetzdesen@gmail.com>
2025-02-17 15:19:59 -0300

    Refatoração da funcionalidade compartilhar projeto

--- src/main/java/com/br/estimativadeprojetodesoftware/command/AbrirCompartilhamentoProjetoCommand.java
+++ src/main/java/com/br/estimativadeprojetodesoftware/command/AbrirCompartilhamentoProjetoCommand.java
@@ -1,19 +1,22 @@
 package com.br.estimativadeprojetodesoftware.command;
 
 import com.br.estimativadeprojetodesoftware.presenter.CompartilharProjetoPresenter;
+import com.br.estimativadeprojetodesoftware.presenter.PrincipalPresenter;
 import com.br.estimativadeprojetodesoftware.presenter.helpers.WindowManager;
+import com.br.estimativadeprojetodesoftware.repository.ProjetoRepositoryMock;
 import com.br.estimativadeprojetodesoftware.repository.UsuarioRepositoryMock;
 import com.br.estimativadeprojetodesoftware.view.CompartilharProjetoView;
 
 import javax.swing.*;
 
 public class AbrirCompartilhamentoProjetoCommand implements ProjetoCommand {
-    private final JDesktopPane desktop;
-    private final UsuarioRepositoryMock repository;
 
-    public AbrirCompartilhamentoProjetoCommand(JDesktopPane desktop, UsuarioRepositoryMock repository) {
+    private final JDesktopPane desktop;
+    private final PrincipalPresenter principalPresenter;
+
+    public AbrirCompartilhamentoProjetoCommand(JDesktopPane desktop, PrincipalPresenter principalPresenter) {
         this.desktop = desktop;
-        this.repository = repository;
+        this.principalPresenter = principalPresenter;
     }
 
     @Override
@@ -25,7 +28,7 @@ public class AbrirCompartilhamentoProjetoCommand implements ProjetoCommand {
             windowManager.bringToFront(tituloJanela);
         } else {
             CompartilharProjetoView compartilharView = new CompartilharProjetoView();
-            new CompartilharProjetoPresenter(compartilharView, repository);
+            new CompartilharProjetoPresenter(compartilharView, principalPresenter);
             compartilharView.setTitle(tituloJanela);
             desktop.add(compartilharView);
             compartilharView.setVisible(true);
